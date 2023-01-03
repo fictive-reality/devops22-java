fe216a136512d4c5eb6ac7dd845768160771d3dd
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-03 10:09:48 +0100

    first commit

---
+++ lesson_1/DebuggHello.java
@@ -0,0 +1,7 @@
+public class DebuggHello {
+    public static void main(String[] args){
+        System.out.println("Hej");
+        System.out.println("Hello");
+        System.out.println("Hola");
+    }
+};
