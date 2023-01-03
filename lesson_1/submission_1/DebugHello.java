f12dda0aab3bf6958fac6068c2ae2ac227878a0c
PapaPeskwo <tomi.orlovac@gmail.com>
2023-01-03 12:50:29 +0100

    Övning 1, klar - Tomislav

---
+++ lesson_1/submission_1/DebugHello.java
@@ -0,0 +1,10 @@
+public class DebugHello {
+
+    public static void main(String[] args) {
+        String name = "Tomi";
+        String yourName = "Tomi";
+
+        System.out.println(name == yourName); 
+        System.out.println(name.equals(yourName));
+    }
+}
