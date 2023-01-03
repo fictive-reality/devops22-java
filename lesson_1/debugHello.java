e0564fceffa33ea85eb64b7dbdce880b93ac533b
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-03 13:32:35 +0100

    Completed exercise 1

---
+++ lesson_1/debugHello.java
@@ -0,0 +1,10 @@
+public class DebugHello {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        String str = "Hello World";
+        char c = '!';
+        System.out.println(str + c);
+        System.out.println(x + y);
+    }
+}
