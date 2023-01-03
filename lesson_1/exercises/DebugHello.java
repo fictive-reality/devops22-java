142c851a6673dd3b6f12ac79c034f67e48c56991
Rickard Lindvist <rickard.lindqvst@yh.nackademin.se>
2023-01-03 13:30:19 +0100

    added exercise solutions

---
+++ lesson_1/exercises/DebugHello.java
@@ -0,0 +1,9 @@
+public class DebugHello {
+    public static void main(String[] args) {
+    for (int i = 1; i<=3; i++) 
+    {System.out.println(i);}
+    System.out.println("Hello!");
+    for (int i=3; i>=1; i--)
+    {System.out.println(i);}
+    }
+}
---
+++ lesson_1/exercises/FirstHello.java
@@ -0,0 +1,8 @@
+package lesson_1.exercises;
+
+public class FirstHello {
+    public static void main(String[] args) {
+        String greeting = "G'day sir!";
+        System.out.println(greeting);
+    }
+}
