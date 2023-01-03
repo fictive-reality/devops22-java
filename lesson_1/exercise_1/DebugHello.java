4e112119ba4ebb447cc9983a1e01d01d26453c0e
David Hasenson <90691386+davidhasenson@users.noreply.github.com>
2023-01-03 17:17:42 +0100

    Added FirstHello.java and DebugHello.java

---
+++ lesson_1/exercise_1/DebugHello.java
@@ -0,0 +1,9 @@
+package lesson_1.exercise_1;
+
+public class DebugHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello debug");
+        System.out.println("Goodbye");
+    }
+}
