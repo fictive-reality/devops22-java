3cfa052d511fad880b2fee2d2d697022230597d2
Nina-se <mattsson-nina@hotmail.com>
2023-01-03 12:41:13 +0100

    My answers_NINAMA

---
+++ lesson_1/min_uppgift/DebugHello.java
@@ -0,0 +1,8 @@
+public class DebugHello {
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello AGAIN!");
+        System.out.println("One more time HELLO!");
+
+    }
+}
