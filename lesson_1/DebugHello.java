ec8f6010190591b6e6a43bd74a196093456bed79
Mario Khalaf <Mario.khalaf9810@gmail.com>
2023-01-03 12:31:28 +0100

    test

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,10 @@
+public class DebugHello {
+    public static void main(String[] args) {
+        String message = "Hello";
+        System.out.println(message);
+        System.out.println("Hello World");
+        System.out.println("Test");
+        System.out.println("Test 2");
+     
+}    
+}
