d57cc4f8b608f0836c51d71c45330f1f18928883
Corn1344 <fabianlrstad@yahoo.se>
2023-01-03 15:12:17 +0100

    Lesson_2

--- lesson_2/exercise/LearnTypes.java
+++ lesson_2/exercise/LearnTypes.java
@@ -8,18 +8,18 @@ class LearnTypes {
 
         // Uncomment & fill in the missing values
         answer.isString("hello world");
-        // answer.isFloat(?);
-        // answer.isInteger(?);
+        answer.isFloat(0.0f);
+        answer.isInteger(1);
 
-        // byte any_byte = ?;
-        // answer.isByte(any_byte);
+        byte any_byte = 1;
+        answer.isByte(any_byte);
 
-        // short any_short = ?;
-        // answer.isShort(any_short);
+        short any_short = 32000;
+        answer.isShort(any_short);
 
-        // answer.isDouble(?);
-        // answer.isLong(?);
-        // answer.isChar(?);
-        // answer.isBoolean(?);
+        answer.isDouble(0.0d);
+        answer.isLong(0L);
+        answer.isChar('\u0000');
+        answer.isBoolean(true);
     }
 }
