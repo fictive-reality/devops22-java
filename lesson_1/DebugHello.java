7669a3c635b40c3413567f1208509b8bda8e0b85
fradop <hugogoransson1@gmail.com>
2023-01-03 13:29:48 +0100

    lesson 1

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,8 @@
+public class DebugHello {
+    public static void main(String[] args)  {
+        System.out.println("Hello World");
+        System.out.println("Tjenare");
+        System.out.println("Hej på dig");
+}
+}
+
