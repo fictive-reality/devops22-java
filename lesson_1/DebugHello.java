81853f31db50b5a8b6083ed0bc4399b5ccab385a
delu2201 <112485718+delu2201@users.noreply.github.com>
2023-01-03 13:02:18 +0100

    Print out Hello World!

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,9 @@
+public class DebugHello {
+    public static void main(String[] args) {
+        String message = "HejHoppsan!";
+        System.out.println(message);
+        System.out.println("Dennis was here!");
+        System.out.println("Dungeon run was one hell of a game!");
+
+    }
+}
