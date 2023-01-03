3a0a2c77133a9a7af3a62a6732fd42e9ab428d38
David Sundgren <sundgren.david@gmail.com>
2023-01-03 12:35:02 +0100

    exercise 1

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,12 @@
+class DebugHello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String hello = "hello world";
+        System.out.println(hello);
+        int i = 20;
+        int j = 10;
+        int x = i + j;
+        System.out.println(i + j);
+        System.out.println(x);
+    }
+}
