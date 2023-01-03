ddfc986395377419e32981f7e1350ea071f945f0
delu2201 <112485718+delu2201@users.noreply.github.com>
2023-01-03 13:13:51 +0100

    Print out Hello World!

--- lesson_1/FirstHello.java
+++ lesson_1/FirstHello.java
@@ -1,7 +1,8 @@
 public class FirstHello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String hello = "Hello World!";
+        System.out.println(hello);
 
     }
 }
