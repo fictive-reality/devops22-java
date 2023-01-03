6a80c03f70549ffd2af14a81795aaedd289fed80
Corn1344 <fabianlrstad@yahoo.se>
2023-01-03 13:23:20 +0100

    First lesson

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,8 @@
+public class DebugHello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hejsan Svejsan");
+        System.out.println("Heja Hammarby");
+        System.out.println("Hejsan");
+    }
+}
