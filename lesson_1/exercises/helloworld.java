a4390488503387a0ec6c46aafba4ce72ff94d1eb
Rickard Lindvist <rickard.lindqvst@yh.nackademin.se>
2023-01-03 13:03:46 +0100

    added file

---
+++ lesson_1/exercises/helloworld.java
@@ -0,0 +1,7 @@
+package lesson_1.exercises
+public class Doorman {
+    public static void main(String[] args) {
+        string greeting = "G'day sir!";
+        System.out.println(greeting);
+    }
+}
