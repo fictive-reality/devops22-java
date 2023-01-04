126c6fde7e2025dd28016c9406e4a44996a2c4a0
Mario Khalaf <Mario.khalaf9810@gmail.com>
2023-01-04 11:05:47 +0100

    finished excercise 2

--- lesson_2/exercise/LearnTypes.java
+++ lesson_2/exercise/LearnTypes.java
@@ -8,18 +8,18 @@ class LearnTypes {
 
         // Uncomment & fill in the missing values
         answer.isString("hello world");
-        // answer.isFloat(?);
-        // answer.isInteger(?);
+        answer.isFloat(1.0f);
+        answer.isInteger(1);
 
-        // byte any_byte = ?;
-        // answer.isByte(any_byte);
+        byte any_byte = 1;
+        answer.isByte(any_byte);
 
-        // short any_short = ?;
-        // answer.isShort(any_short);
+        short any_short = 1;
+        answer.isShort(any_short);
 
-        // answer.isDouble(?);
-        // answer.isLong(?);
-        // answer.isChar(?);
-        // answer.isBoolean(?);
+        answer.isDouble(2.0d);
+        answer.isLong(1L);
+        answer.isChar('A');
+        answer.isBoolean(true);
     }
 }
