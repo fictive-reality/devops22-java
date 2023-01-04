f5215c333e77e2b94d80d8b062b57268f95f3b47
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-04 11:07:08 +0100

    lesson 2

--- lesson_2/exercise/LearnTypesMinMax.java
+++ lesson_2/exercise/LearnTypesMinMax.java
@@ -9,32 +9,32 @@ class LearnTypesMinMax {
         // Find the min/max values in docs
         // https://docs.oracle.com/javase/specs/jls/se17/html/jls-3.html#jls-IntegerLiteral
         answer.isMaximumInteger(2147483647);
-        // answer.isMinimumInteger(?);
+        answer.isMinimumInteger(0x8000_0000);
 
         // Find the floating-point literal in docs
         // https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/Float.html
-        // answer.isMinimumFloat(?);
-        // answer.isMaximumFloat(?);
+        answer.isMinimumFloat(0x1.0P-149f);
+        answer.isMaximumFloat(0x1.f_ffffeP+127f);
 
         // Find the double min/max in docs, navigate from the Float page
-        // answer.isMinimumDouble(?);
-        // answer.isMaximumDouble(?);
+        answer.isMinimumDouble(0x1.0P-1074d);
+        answer.isMaximumDouble(0x1.f_ffff_ffff_ffffP+1023d);
 
         // Find the byte min/max
-        // byte min = ?;
-        // byte max = ?;
-        // answer.isMinimumByte(min);
-        // answer.isMaximumByte(max);
+        byte min = -0x80;
+        byte max = 0x7f;
+        answer.isMinimumByte(min);
+        answer.isMaximumByte(max);
 
         // Find the short min/max
-        // short short_min = ?;
-        // short short_max = ?;
-        // answer.isMinimumShort(short_min);
-        // answer.isMaximumShort(short_max);
+        short short_min = -0x8000;
+        short short_max = 0x7fff;
+        answer.isMinimumShort(short_min);
+        answer.isMaximumShort(short_max);
 
         // Find the long min/max
         // https://docs.oracle.com/javase/specs/jls/se17/html/jls-4.html#jls-4.2.1
-        // answer.isMinimumLong(?);
-        // answer.isMaximumLong(?);
+        answer.isMinimumLong(-0x8000_0000_0000_0000L);
+        answer.isMaximumLong(0x7fff_ffff_ffff_ffffL);
     }
 }
