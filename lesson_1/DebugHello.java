76ef61125d84f11227b127a58f69bf36c8e1b8ac
manibadani <manimandana@hotmail.com>
2023-01-05 20:34:42 +0100

    lesson_1

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,8 @@
+class DebugHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello You!");
+        System.out.println("Hello Me!");
+    }
+}
