59901900030fcb766cc8b5e00e4621c4b5780ed8
[Gustav Öberg] <[gustav.oberg96@gmail.com]>
2023-01-09 16:51:29 +0100

    Lesson 1 Exercises Done

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,7 @@
+public class DebugHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Another line!");
+        System.out.println("And another line!");
+        }
+}
