b3b57e8dddca4b0a4a82eba6ce15486b258f219a
[Gustav Öberg] <[gustav.oberg96@gmail.com]>
2023-01-09 16:39:06 +0100

    Lesson 2 exercise done

--- lesson_2/exercise/LearnTypes.java
+++ lesson_2/exercise/LearnTypes.java
@@ -8,18 +8,18 @@ class LearnTypes {
 
         // Uncomment & fill in the missing values
         answer.isString("hello world");
-        // answer.isFloat(?);
-        // answer.isInteger(?);
-
-        // byte any_byte = ?;
-        // answer.isByte(any_byte);
-
-        // short any_short = ?;
-        // answer.isShort(any_short);
-
-        // answer.isDouble(?);
-        // answer.isLong(?);
-        // answer.isChar(?);
-        // answer.isBoolean(?);
+        answer.isFloat(0.5f);
+        answer.isInteger(1);
+    
+        byte any_byte = 100;
+        answer.isByte(any_byte);
+    
+        short any_short = 5000;
+        answer.isShort(any_short);
+    
+        answer.isDouble(19.99d);
+        answer.isLong(15000000000L);
+        answer.isChar('B');
+        answer.isBoolean(true);
     }
 }
