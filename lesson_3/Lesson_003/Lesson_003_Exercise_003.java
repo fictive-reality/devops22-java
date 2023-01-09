d8a6dcbfd81e4e808a77ecb8de0865f225e34e3c
Timmy Elf <Timmy.Elf@th.nackademin.se>
2023-01-09 21:30:13 +0100

    Updated lesson 003.

--- lesson_3/Lesson_003/Lesson_003_Exercise_003.java
+++ lesson_3/Lesson_003/Lesson_003_Exercise_003.java
@@ -1,27 +1,25 @@
-package lesson_3.examples;
+package lesson_3.examples.Lesson_003;
 
-import java.util.Random;
-
-public class D_BreakExample {
-    static final int LIMIT = 25;
-
-    // Individual Exercise: Write a `while` that uses `break`.
-    public static void main(String[] args) throws InterruptedException {
-        Random random = new Random();
-        int luckyNumber = random.nextInt(LIMIT);
-        System.out.printf("My lucky number is: %d\n", luckyNumber);
-
-        // This while loop will run until the lucky number is found.
-        while (true) {
-            int randomNumber = random.nextInt(LIMIT);
-
-            // We can use == since it's a primitive variable int
-            if (randomNumber == luckyNumber) {
-                System.out.printf("You found my lucky number!!! (%d)\n", luckyNumber);
-                break; // break will end the while loop
-            }
-            System.out.printf("%d is not my lucky number \n", randomNumber);
-            Thread.sleep(1000);
+public class Lesson_003_Exercise_003 {
+    
+    int i = 0;
+    while (i < 96) {
+        System.out.println(i);
+        i++;
+        if (i == 89) {
+            break;
         }
     }
-}
+
+    int i = 0;
+    while (i < 10) {
+        if (i == 4) {
+        i++;
+        continue;
+    }
+        System.out.println(i);
+        i++;
+
+    
+    }
+}}
