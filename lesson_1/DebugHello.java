84607c65aa2cfd2a2da2bc7a9e0017a155dffbe2
Oskar Tölli <oskartolli@hotmail.com>
2023-01-09 10:32:46 +0100

    Added hello world debug

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,10 @@
+public class DebugHello {
+    public static void main(String[] args) {
+        String x = "Hello...";
+        String y = "This is a line of text.";
+        String z = "Testy test text.";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
