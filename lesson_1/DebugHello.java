36b3dac725f6c37d2f366e96f24b46f1cdca20bb
Isac Grive <isac.grive@gmail.com>
2023-01-09 13:19:57 +0100

    Screenshots and java applications added

---
+++ lesson_1/DebugHello.java
@@ -0,0 +1,8 @@
+public class DebugHello {
+    public static void main(String[] args) {
+
+        System.out.println("Debug World");
+        System.out.println("Testing...");
+        System.out.println("Java is nice");
+    }
+}
