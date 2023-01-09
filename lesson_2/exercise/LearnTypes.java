0c748248fd7231cc90aba510c96a63747cf9cd31
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-09 09:27:55 +0100

    completed exercise 2

--- lesson_2/exercise/LearnTypes.java
+++ lesson_2/exercise/LearnTypes.java
@@ -8,18 +8,18 @@ class LearnTypes {
 
         // Uncomment & fill in the missing values
         answer.isString("hello world");
-        // answer.isFloat(?);
-        // answer.isInteger(?);
+        answer.isFloat(5.5f);
+        answer.isInteger(5);
 
-        // byte any_byte = ?;
-        // answer.isByte(any_byte);
+        byte any_byte = 101;
+        answer.isByte(any_byte);
 
-        // short any_short = ?;
-        // answer.isShort(any_short);
+        short any_short = 5;
+        answer.isShort(any_short);
 
-        // answer.isDouble(?);
-        // answer.isLong(?);
-        // answer.isChar(?);
-        // answer.isBoolean(?);
+        answer.isDouble(5.5d);
+        answer.isLong(5L);
+        answer.isChar('!');
+        answer.isBoolean(false);
     }
 }
--- lesson_2/exercise/LearnTypesMinMax.java
+++ lesson_2/exercise/LearnTypesMinMax.java
@@ -9,32 +9,31 @@ class LearnTypesMinMax {
         // Find the min/max values in docs
         // https://docs.oracle.com/javase/specs/jls/se17/html/jls-3.html#jls-IntegerLiteral
         answer.isMaximumInteger(2147483647);
-        // answer.isMinimumInteger(?);
+        answer.isMinimumInteger(-2147483648);
 
         // Find the floating-point literal in docs
         // https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/Float.html
-        // answer.isMinimumFloat(?);
-        // answer.isMaximumFloat(?);
-
+        answer.isMinimumFloat((float) Math.pow(2, (-149)));
+        answer.isMaximumFloat((float) (2 - Math.pow(2, -23)) * (float) Math.pow(2, 127));
         // Find the double min/max in docs, navigate from the Float page
-        // answer.isMinimumDouble(?);
-        // answer.isMaximumDouble(?);
+        answer.isMinimumDouble((double) Math.pow(2, (-1074)));
+        answer.isMaximumDouble((double) (2 - Math.pow(2, -52)) * (double) Math.pow(2, 1023));
 
         // Find the byte min/max
-        // byte min = ?;
-        // byte max = ?;
-        // answer.isMinimumByte(min);
-        // answer.isMaximumByte(max);
+        byte min = -128;
+        byte max = 127;
+        answer.isMinimumByte(min);
+        answer.isMaximumByte(max);
 
         // Find the short min/max
-        // short short_min = ?;
-        // short short_max = ?;
-        // answer.isMinimumShort(short_min);
-        // answer.isMaximumShort(short_max);
+        short short_min = -32768;
+        short short_max = 32767;
+        answer.isMinimumShort(short_min);
+        answer.isMaximumShort(short_max);
 
         // Find the long min/max
         // https://docs.oracle.com/javase/specs/jls/se17/html/jls-4.html#jls-4.2.1
-        // answer.isMinimumLong(?);
-        // answer.isMaximumLong(?);
+        answer.isMinimumLong(-9223372036854775808L);
+        answer.isMaximumLong(9223372036854775807L);
     }
 }
