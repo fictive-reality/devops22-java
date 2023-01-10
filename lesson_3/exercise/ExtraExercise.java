1943bf9d9dcdbf4e1872000cee48769762bc7a71
David Hasenson <90691386+davidhasenson@users.noreply.github.com>
2023-01-10 09:03:36 +0100

    mer

--- lesson_3/exercise/ExtraExercise.java
+++ lesson_3/exercise/ExtraExercise.java
@@ -5,41 +5,45 @@ public class ExtraExercise {
     int number;
     float radius;
 
-    public void response() {
+    public void calculate() {
 
         while (true) {
-            // System.out.println("Enter radius: ");
-            try (Scanner sc = new Scanner(System.in)) {
-                System.out.println("Enter radius: ");
-                this.radius = sc.nextFloat();
-                System.out.println("Enter 1: area, 2:circumference, 0:exit ");
-                this.number = sc.nextInt();
-            }
-
-            switch (number) {
-                case 0: {
-                    System.out.println("Exit program. ");
-                    break;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter radius: ");
+            this.radius = sc.nextFloat();
+            System.out.println("Enter 1: area, 2:circumference, 0:Exit ");
+            this.number = sc.nextInt();
+            // fungerar inte. vet inte varför.
+            if (this.number > 0 && this.number < 100) {
+                switch (this.number) {
+                    case 1: {
+                        float area = (float) (Math.PI * Math.pow(this.radius, 2));
+                        System.out.println(area);
+                        continue;
+                    }
+                    case 2: {
+                        float circumference = (float) (2 * Math.PI * this.radius);
+                        System.out.println(circumference);
+                        continue;
+                    }
+                    default:
+                        System.out.println("Not an option. ");
                 }
-                case 1: {
-                    float area = (float) (Math.PI * Math.pow(this.radius, 2));
-                    System.out.printf("area", area);
-                    continue;
-                }
-                case 2: {
-                    float circumference = (float) (2 * Math.PI * this.radius);
-                    System.out.printf("circumference  \n", circumference);
-                    continue;
-                }
-                default:
-                    System.out.println("Not an option. ");
-
+            } else if (this.number < 0) {
+                System.out.println(" guess a higher number ");
+                continue;
+            } else if (this.number == 0) {
+                System.out.println("Exit ");
+                break;
+            } else {
+                continue;
             }
         }
+
     }
 
     public static void main(String[] args) {
         ExtraExercise extraExercise = new ExtraExercise();
-        extraExercise.response();
+        extraExercise.calculate();
     }
 }
