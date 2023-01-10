743b96c9a3be989e03d5f4af4af3e0c73dc16768
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-10 15:04:24 +0100

    Zoo

---
+++ lesson_4/Zoo/Lemur.java
@@ -0,0 +1,16 @@
+class Lemur implements Animal {
+	private String name = "Lemur";
+	private int age = 0;
+	private int fluffiness = 4;
+
+	public String getName() { return this.name; }
+	public int getAge() { return this.age; }
+	public int getFluffiness() { return this.fluffiness; }
+
+	public String speak() { return "Hakkunah Matatatatatatata"; }
+	public void backflip() {}
+
+	public void eat() {}
+	public void sleep() {}
+	public void snuggle() {}
+}
