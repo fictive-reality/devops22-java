f73b817715ae0306b79945e427849a77e3adfbe2
Timmy Elf <Timmy.Elf@yh.nackademin.se>
2023-01-12 10:49:35 +0100

    Updated exercise.

--- lesson_4/src/Lesson004/Enclosure.java
+++ lesson_4/src/Lesson004/Enclosure.java
@@ -1,5 +1,15 @@
 package Lesson004;
 
-public class Enclosure {
-    
+class Enclosure {
+	private double size; // in sq ft
+	private bool dirty;
+	private double temperature;
+
+	public double getSize() { return this.size; }
+	public bool getDirty() { return this.dirty; }
+	public double getTemperature() { return this.temperature; }
+
+	public void clean() {}
+	public void close() {}
+	public void open() {}
 }
--- lesson_4/src/Lesson004/Lemur.java
+++ lesson_4/src/Lesson004/Lemur.java
@@ -1,5 +1,18 @@
 package Lesson004;
 
-public class Lemur {
-    
+class Lemur implements Animal {
+	private String name = "Lemur";
+	private int age = 0;
+	private int fluffiness = 4;
+
+	public String getName() { return this.name; }
+	public int getAge() { return this.age; }
+	public int getFluffiness() { return this.fluffiness; }
+
+	public String speak() { return "Hakkunah Matatatatatatata"; }
+	public void backflip() {}
+
+	public void eat() {}
+	public void sleep() {}
+	public void snuggle() {}
 }
