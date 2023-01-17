d624858f176c5917643070b77d8641f18f4cf2f7
Fredrik Magnusson <raekon92@gmail.com>
2023-01-17 19:29:16 +0100

    added modified files

--- lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 
 class JDBCUtils {
     String hostname = "localhost";
@@ -26,9 +28,10 @@ class JDBCUtils {
 
     public void createDatabase(Connection conn, String name) throws SQLException {
         String createString = "create database IF NOT EXISTS " + name;
-
+        String useString = "USE " + name;
         try (Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(createString);
+            stmt.executeUpdate(useString);
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -43,9 +46,110 @@ class JDBCUtils {
         connectionProps.put("password", this.password);
 
         // Modify the "/" after this.port to set a specific database
-        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/", connectionProps);
+        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/",
+                connectionProps);
 
         System.out.println("Connected to db");
         return conn;
     }
+
+    public void createTable(Connection conn, String name) throws SQLException {
+        String createString = "create table IF NOT EXISTS " + name + " (id int, name varchar(255))";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void insertData(Connection conn, String name, int id, String name2) throws SQLException {
+        String createString = "insert into " + name + " (id, name) values (?, ?)";
+
+        try (PreparedStatement stmt = conn.prepareStatement(createString)) {
+            stmt.setInt(1, id);
+            stmt.setString(2, name2);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void readData(Connection conn, String name) throws SQLException {
+        String createString = "select * from " + name;
+
+        try (PreparedStatement stmt = conn.prepareStatement(createString)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void selectData(Connection conn, String name) throws SQLException {
+        String createString = "select * from " + name;
+
+        try (PreparedStatement stmt = conn.prepareStatement(createString)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void updateData(Connection conn, String name, int id, String name2) throws SQLException {
+        String createString = "update " + name + " set name = ? where id = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(createString)) {
+            stmt.setString(1, name2);
+            stmt.setInt(2, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void deleteData(Connection conn, String name, int id) throws SQLException {
+        String createString = "delete from " + name + " where id = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(createString)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void dropTable(Connection conn, String name) throws SQLException {
+        String createString = "drop table " + name;
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void dropDatabase(Connection conn, String name) throws SQLException {
+        String createString = "drop database " + name;
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
--- lesson_7/src/main/java/se/nackademin/jdbc/Main.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/Main.java
@@ -8,9 +8,29 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         JDBCUtils jdbcUtils = new JDBCUtils("localhost");
         jdbcUtils.setUsername("root"); // Never use the root user in real apps
-        jdbcUtils.setPassword("my-secret-pw"); // Never add hardcoded passwords to your code
+        jdbcUtils.setPassword("example"); // Never add hardcoded passwords to your code
 
         Connection conn = jdbcUtils.getConnection();
-        jdbcUtils.createDatabase(conn, "example"); // This will create the database with no tables
+        jdbcUtils.createDatabase(conn, "devops22"); // This will create the database with no tables
+
+        jdbcUtils.createTable(conn, "testTable"); // This will create the table with no data
+
+        jdbcUtils.insertData(conn, "testTable", 1, "devops-22"); // This will insert data into the table
+
+        jdbcUtils.readData(conn, "testTable"); // This will read data from the table
+
+        jdbcUtils.updateData(conn, "testTable", 1, "devops-22-updated"); // This will update data in the table
+
+        jdbcUtils.readData(conn, "testTable"); // This will read data from the table
+
+        jdbcUtils.deleteData(conn, "testTable", 1); // This will delete data from the table
+
+        jdbcUtils.readData(conn, "testTable"); // This will read data from the table
+
+        // jdbcUtils.dropTable(conn, "testTable"); // This will drop the table
+
+        // jdbcUtils.dropDatabase(conn, "devops22"); // This will drop the database
+
+        conn.close();
     }
 }
