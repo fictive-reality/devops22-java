46c86ffdcf5209506f2827d651f174a993664f93
Martin Alfredson <martin.alfredson@hotmail.com>
2023-01-17 18:00:59 +0100

    Main calling the CRUD methods

--- lesson_7/src/main/java/se/nackademin/jdbc/Main.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/Main.java
@@ -8,9 +8,25 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         JDBCUtils jdbcUtils = new JDBCUtils("localhost");
         jdbcUtils.setUsername("root"); // Never use the root user in real apps
-        jdbcUtils.setPassword("my-secret-pw"); // Never add hardcoded passwords to your code
+        jdbcUtils.setPassword("admin"); // Never add hardcoded passwords to your code
 
         Connection conn = jdbcUtils.getConnection();
-        jdbcUtils.createDatabase(conn, "example"); // This will create the database with no tables
+
+        // Creates databbase EXAMPLE and table INVENTORY
+        jdbcUtils.createDatabase(conn, "example");
+
+        // All bananas and papayas with price 1 and 2 are deleted
+        JDBCUtils.deleteFromTable(conn, "example", "banana", 1);
+        JDBCUtils.deleteFromTable(conn, "example", "papaya", 2);
+
+        // All bananas and papayas from previous time app ran have their price set to 1 and 2
+        JDBCUtils.updateTable(conn, "example", "banana", 1);
+        JDBCUtils.updateTable(conn, "example", "papaya", 2);
+
+        // Adds new fruits to the table
+        jdbcUtils.populateTable(conn, "example");
+
+        // Prints table INVENTORY
+        JDBCUtils.viewTable(conn, "example");
     }
 }
