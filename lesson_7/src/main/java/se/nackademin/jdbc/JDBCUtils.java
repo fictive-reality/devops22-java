27c75da1a625aec3fdc53e8dfb40f3ab7c056600
David Sundgren <sundgren.david@gmail.com>
2023-01-18 10:53:05 +0100

    exercise 7

--- lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
@@ -2,14 +2,15 @@ package se.nackademin.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
 class JDBCUtils {
-    String hostname = "localhost";
-    String userName = "username";
-    String password = "password"; // Never add hardcoded passwords to your code
+    String hostname; // = "localhost";
+    String userName; // = "username";
+    String password; // = "password"; // Never add hardcoded passwords to your code
     String port = "3306";
 
     JDBCUtils(String hostname) {
@@ -43,9 +44,68 @@ class JDBCUtils {
         connectionProps.put("password", this.password);
 
         // Modify the "/" after this.port to set a specific database
-        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/", connectionProps);
+        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/test",
+                connectionProps);
 
         System.out.println("Connected to db");
         return conn;
+    }
+
+    public void createTable(Connection conn) throws SQLException {
+
+        String create = "CREATE TABLE IF NOT EXISTS cars" + "(name VARCHAR(10)," + "model VARCHAR(10),"
+                + "year INTEGER)";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(create);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insertIntoTable(Connection conn) throws SQLException {
+        String insert = "INSERT INTO cars(name, model, year)" + "VALUES('Audi', 'A6', 2020)";
+        String insert2 = "INSERT INTO cars(name, model, year)" + "VALUES('Volvo', 'V60', 2022)";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(insert);
+            stmt.executeUpdate(insert2);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void selectAll(Connection conn) throws SQLException {
+        String select = "SELECT * FROM cars";
+
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(select);
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String model = rs.getString("model");
+                int year = rs.getInt("year");
+                System.out.println(name + " " + model + " " + year);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateValue(Connection conn) throws SQLException {
+        String update = "UPDATE cars SET year = 2023 WHERE name = 'Audi'";
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(update);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteRow(Connection conn) throws SQLException {
+        String update = "DELETE FROM cars WHERE model = 'V60'";
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(update);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 }
