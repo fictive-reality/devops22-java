3359a20825e66f06c3585f276f3e8071829167b4
Saman Petfat <Saman.petfat@yh.nackademin.se>
2023-01-18 12:38:07 +0100

    lesson 7

--- lesson_7/src/main/java/se/nackademin/jdbc/Main.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/Main.java
@@ -1,6 +1,7 @@
 package se.nackademin.jdbc;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Connection;
 
 public class Main {
@@ -8,9 +9,14 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         JDBCUtils jdbcUtils = new JDBCUtils("localhost");
         jdbcUtils.setUsername("root"); // Never use the root user in real apps
-        jdbcUtils.setPassword("my-secret-pw"); // Never add hardcoded passwords to your code
+        jdbcUtils.setPassword("example"); // Never add hardcoded passwords to your code
 
         Connection conn = jdbcUtils.getConnection();
-        jdbcUtils.createDatabase(conn, "example"); // This will create the database with no tables
+        jdbcUtils.createDatabase(conn, "test"); // This will create the database with no tables
+        // jdbcUtils.createTable();
+        // jdbcUtils.insertInto();
+        // jdbcUtils.dropTable();
+        jdbcUtils.selectFromDatabase();
+        // jdbcUtils.deleteFromDatabase();
     }
 }
