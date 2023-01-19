a4ea11544b2b8d2f0ff824e616fa8d13792ece8d
Odd Jensen <Odd.Jensen@yh.nackademin.se>
2023-01-19 10:41:24 +0100

    completed exercise 7

--- lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/JDBCUtils.java
@@ -2,7 +2,10 @@ package se.nackademin.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -35,6 +38,88 @@ class JDBCUtils {
 
     }
 
+    public void createTable(Connection con) throws SQLException {
+        String createString = "create table IF NOT EXISTS Lulmane " +
+                "(USER_NAME varchar(40) NOT NULL, " +
+                "DKP int NOT NULL DEFAULT 0, " +
+                "PRIMARY KEY (USER_NAME))";
+
+        try (Statement stmt = con.createStatement()) {
+            stmt.executeUpdate(createString);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void deleteTable(Connection con) throws SQLException {
+        String createString = "DROP TABLE Lulmane";
+
+        try (Statement stmt = con.createStatement()) {
+            stmt.executeUpdate(createString);
+            System.out.println("Dwleted the table");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void addUser(Connection conn, String name) throws SQLException {
+        String updateString = "INSERT INTO Lulmane(USER_NAME, DKP) " +
+                "VALUES (?, 0)";
+
+        try (PreparedStatement updateDkp = conn.prepareStatement(updateString);) {
+            updateDkp.setString(1, name);
+            updateDkp.executeUpdate();
+            System.out.println("Added " + name + " to the Guild");
+        } catch (SQLIntegrityConstraintViolationException i) {
+            System.out.println("User named " + name + " already exists");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void getAllUsers(Connection con) throws SQLException {
+        String createString = "SELECT USER_NAME, DKP FROM Lulmane";
+
+        try (Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery(createString);
+            while (rs.next()) {
+                String userName = rs.getString("USER_NAME");
+                int dkp = rs.getInt("DKP");
+                System.out.println(userName + " has " + dkp + " DKP");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void giveDKP(Connection conn, String name, int dkp) throws SQLException {
+        String updateString = "update Lulmane " + "set DKP = DKP+? where USER_NAME = ?";
+
+        try (PreparedStatement updateDkp = conn.prepareStatement(updateString);) {
+            updateDkp.setInt(1, dkp);
+            updateDkp.setString(2, name);
+            updateDkp.executeUpdate();
+            System.out.println("Gave " + name + " " + dkp + " DKP");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void kickUser(Connection conn, String name) throws SQLException {
+        String updateString = "DELETE FROM Lulmane " + "where USER_NAME =?";
+
+        try (PreparedStatement updateDkp = conn.prepareStatement(updateString);) {
+            updateDkp.setString(1, name);
+            updateDkp.executeUpdate();
+            System.out.println("Kicked " + name + " from the Guild");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
     public Connection getConnection() throws SQLException {
 
         Connection conn = null;
@@ -43,7 +128,8 @@ class JDBCUtils {
         connectionProps.put("password", this.password);
 
         // Modify the "/" after this.port to set a specific database
-        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/", connectionProps);
+        conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/example",
+                connectionProps);
 
         System.out.println("Connected to db");
         return conn;
--- lesson_7/src/main/java/se/nackademin/jdbc/Main.java
+++ lesson_7/src/main/java/se/nackademin/jdbc/Main.java
@@ -8,9 +8,14 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         JDBCUtils jdbcUtils = new JDBCUtils("localhost");
         jdbcUtils.setUsername("root"); // Never use the root user in real apps
-        jdbcUtils.setPassword("my-secret-pw"); // Never add hardcoded passwords to your code
+        jdbcUtils.setPassword("test"); // Never add hardcoded passwords to your code
 
         Connection conn = jdbcUtils.getConnection();
-        jdbcUtils.createDatabase(conn, "example"); // This will create the database with no tables
+        jdbcUtils.createDatabase(conn, "example"); // This will create the database
+        // with no tables
+        jdbcUtils.createTable(conn);
+        // jdbcUtils.kickUser(conn, "Zues");
+        jdbcUtils.giveDKP(conn, "Dotfisk", 50);
+        jdbcUtils.getAllUsers(conn);
     }
 }
